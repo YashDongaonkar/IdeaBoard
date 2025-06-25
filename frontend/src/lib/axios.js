95a276a6ce95f176746672d8be4e3395dc4594dd
YashDongaonkar <yashdongaonkar94@gmail.com>
2025-06-25 16:34:14 +0530

    configure .env in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const api = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:process.env.BASE_URL
 })
 
 export default api
